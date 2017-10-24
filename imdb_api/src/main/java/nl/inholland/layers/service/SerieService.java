70527bb7d8cd3c465977efab37f41bd8385b9700
JeroenRoos <jeroen.roos8@hotmail.com>
2017-10-24 16:29:46 +0200

    Post Serie

--- imdb_api/src/main/java/nl/inholland/layers/service/SerieService.java
+++ imdb_api/src/main/java/nl/inholland/layers/service/SerieService.java
@@ -14,8 +14,10 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
+import nl.inholland.layers.model.Director;
 import nl.inholland.layers.model.Serie;
 import nl.inholland.layers.persistence.SerieDAO;
+import org.bson.types.ObjectId;
 
 /**
  *
@@ -37,6 +39,8 @@ public class SerieService extends BaseService
     {
         // Game of Thrones  : 59e5d711df5ac0534fbe1670
         // Mr. Robot        : 59e5d463df5ac0534fbde725
+        // South park       : 59ef4baf6384632a4047d441
+        
         Serie serie = serieDAO.get(serieId);
         requireResult(serie, "Serie not found");
         return serie;
@@ -52,6 +56,13 @@ public class SerieService extends BaseService
     @POST
     public void create(Serie serie)
     {
+         //if(ObjectId.isValid(serie.getDirectors().))
+                 
+        for (Director d : serie.getDirectors())
+        {
+            if (ObjectId.isValid(d.getId().toString()));
+        }
+        
         serieDAO.create(serie);
     }
     
