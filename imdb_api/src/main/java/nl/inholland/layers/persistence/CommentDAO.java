7b8fb97b36be0a9fc838337481134184bec9ed29
Remy Kroese <rkroese96@gmail.com>
2017-10-29 21:27:21 +0100

    movie combination GET finished

--- imdb_api/src/main/java/nl/inholland/layers/persistence/CommentDAO.java
+++ imdb_api/src/main/java/nl/inholland/layers/persistence/CommentDAO.java
@@ -8,6 +8,7 @@ package nl.inholland.layers.persistence;
 import java.util.List;
 import javax.inject.Inject;
 import nl.inholland.layers.model.Comment;
+import nl.inholland.layers.model.User;
 import org.bson.types.ObjectId;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.query.Query;
@@ -29,5 +30,13 @@ public class CommentDAO extends BaseDAO<Comment>
     public void deleteManyById(List<ObjectId> lstObjects) {
         Query<Comment> query = ds.createQuery(Comment.class);
         ds.delete(query.filter("_id in", lstObjects));
+    }
+    
+    public List<Comment> getByUser(User user){
+        Query<Comment> query = ds.createQuery(Comment.class);
+        
+        query.filter("users in", user);
+        List<Comment> comments = query.asList();
+        return comments;
     }
 }
